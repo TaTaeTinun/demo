ca8d0ac32de88cd22ffe386c5f28b5b88573e530
Suphachai Kongpakdee <tatae@Suphachais-MacBook-Air.local>
2025-05-19 17:48:21 +0700

    first commit

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
 
-func setupApp() *fiber.App {
-	app := fiber.New()
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Fiber!")
-	})
-	return app
-}
-
 func main() {
-	fmt.Println("Hello, world!")
-	app := setupApp()
+	app := fiber.New()
+
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
 	app.Listen(":80")
 }
